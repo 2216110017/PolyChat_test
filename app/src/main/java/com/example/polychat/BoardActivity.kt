96d92e423912a88563f21a293a46af8e1ff62dc5
2216110017 <i19162013@gmail.com>
2023-08-27 14:36:07 +0900

    7

--- app/src/main/java/com/example/polychat/BoardActivity.kt
+++ app/src/main/java/com/example/polychat/BoardActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.database.ValueEventListener
 
 class BoardActivity : AppCompatActivity() {
 
+    // Firebase 데이터베이스 참조 변수 선언
     private lateinit var database: FirebaseDatabase
     private lateinit var postsRef: DatabaseReference
 
@@ -21,14 +22,18 @@ class BoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
 
+        // 로그인한 사용자의 ID 가져오기
         val userID = intent.getIntExtra("userID", -1)
 
+        // UI 요소 참조 가져오기
         val listViewPosts = findViewById<ListView>(R.id.lvPosts)
         val btnWritePost = findViewById<Button>(R.id.btnWritePost)
 
+        // Firebase 초기화
         database = FirebaseDatabase.getInstance()
         postsRef = database.getReference("users").child(userID.toString()).child("post")
 
+        // 게시글 작성 버튼 클릭 리스너 설정
         btnWritePost.setOnClickListener {
             // 게시글 작성 화면으로 이동
             val intent = Intent(this, WritePostActivity::class.java)
@@ -36,23 +41,41 @@ class BoardActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // 게시글의 unique key를 저장하기 위한 리스트
+        val postKeys = mutableListOf<String>()
+
+        // Firebase에서 게시글 목록 가져오기
         postsRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val postsList = mutableListOf<String>()
+                postKeys.clear() // 리스트 초기화
+
+                // 각 게시글에 대해
                 for (postSnapshot in dataSnapshot.children) {
                     val post = postSnapshot.getValue(WritePostActivity.Post::class.java)
                     postsList.add(post?.title ?: "No title")
+                    postKeys.add(postSnapshot.key ?: "") // 게시글의 unique key 추가
                 }
 
+                // 게시글 목록을 화면에 표시하기 위한 ArrayAdapter 설정
                 val adapter = ArrayAdapter(this@BoardActivity, android.R.layout.simple_list_item_1, postsList)
                 listViewPosts.adapter = adapter
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // Handle error
+                // 데이터 로드 실패 시 에러 처리
             }
         })
+
+        // 게시글 목록의 아이템 클릭 리스너 설정
+        listViewPosts.setOnItemClickListener { _, _, position, _ ->
+            val intent = Intent(this@BoardActivity, PostDetailActivity::class.java)
+            intent.putExtra("postID", postKeys[position]) // 선택된 게시글의 unique key 전달
+            intent.putExtra("userID", userID)
+            startActivity(intent)
+        }
     }
 }
+
 
 
--- app/src/main/java/com/example/polychat/WritePostActivity.kt
+++ app/src/main/java/com/example/polychat/WritePostActivity.kt
@@ -2,21 +2,21 @@ package com.example.polychat
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class WritePostActivity : AppCompatActivity() {
+
     data class Post(
-        val title: String,
-        val content: String,
-        val notice: Boolean
-    )   //게시글 정보 저장용 데이터클래스
+        val title: String = "",
+        val content: String = "",
+        val notice: Boolean = false,
+        val userID: Int = -1
+    )
 
     private lateinit var database: FirebaseDatabase
     private lateinit var postRef: DatabaseReference
@@ -25,48 +25,41 @@ class WritePostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_post)
 
-        // 사용자 ID와 게시글 ID를 가져옴
         val userID = intent.getIntExtra("userID", -1)
         val postID = intent.getStringExtra("postID")
         val editMode = intent.getBooleanExtra("editMode", false)
 
-        val editTextTitle = findViewById<EditText>(R.id.editTextTitle)
-        val editTextContent = findViewById<EditText>(R.id.editTextContent)
-        val btnPost = findViewById<Button>(R.id.btnPost)
+        val editTextTitle = findViewById<EditText>(R.id.editTextTitle)  //제목
+        val editTextContent = findViewById<EditText>(R.id.editTextContent)  //내용
+        val checkboxNotice = findViewById<CheckBox>(R.id.checkBoxNotice)   //공지체크박스
+        val btnPost = findViewById<Button>(R.id.btnPost)    //'게시'
 
-        // Firebase초기화
         database = FirebaseDatabase.getInstance()
         postRef = database.getReference("users").child(userID.toString()).child("post")
 
-
-        // 수정 모드일 경우 기존 게시글 정보를 가져와 화면에 표시
         if (editMode) {
-            postRef.child(postID!!).addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val post = dataSnapshot.getValue(Post::class.java)
-                    editTextTitle.setText(post?.title)
-                    editTextContent.setText(post?.content)
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    Toast.makeText(this@WritePostActivity, "데이터 로드 실패: ${databaseError.message}", Toast.LENGTH_SHORT).show()
-                }
-            })
+            postRef.child(postID!!).get().addOnSuccessListener {
+                val post = it.getValue(Post::class.java)
+                editTextTitle.setText(post?.title)
+                editTextContent.setText(post?.content)
+                checkboxNotice.isChecked = post?.notice ?: false
+            }.addOnFailureListener {
+                Toast.makeText(this, "데이터 로드 실패", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        //'게시'
         btnPost.setOnClickListener {
             val title = editTextTitle.text.toString()
             val content = editTextContent.text.toString()
-            // 제목과 내용이 입력되지 않았을 경우 메시지 표시
+            val isNotice = checkboxNotice.isChecked
+
             if (title.isBlank() || content.isBlank()) {
                 Toast.makeText(this, "제목과 내용을 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val post = Post(title, content, false) // // 공지사항 체크 추가
+            val post = Post(title, content, isNotice, userID)
 
-            // 수정 모드일 경우 게시글을 수정
             if (editMode) {
                 postRef.child(postID!!).setValue(post).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
@@ -76,7 +69,7 @@ class WritePostActivity : AppCompatActivity() {
                         Toast.makeText(this, "게시글 수정 실패.", Toast.LENGTH_SHORT).show()
                     }
                 }
-            } else {    //수정 모드가 아니면 게시글 작성
+            } else {
                 postRef.push().setValue(post).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         Toast.makeText(this, "게시글이 작성되었습니다.", Toast.LENGTH_SHORT).show()
@@ -89,6 +82,7 @@ class WritePostActivity : AppCompatActivity() {
         }
     }
 }
+
 
 
 
